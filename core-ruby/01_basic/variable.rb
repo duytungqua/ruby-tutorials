4615d7c9c7ece80781d3b5be436c2b623677bb67
duytungqua <quangduy.hcmut@gmail.com>
2025-06-05 23:33:25 +0700

    feat: add more type of variable

--- core-ruby/01_basic/variable.rb
+++ core-ruby/01_basic/variable.rb
@@ -7,4 +7,11 @@ puts "Hello, #{name}"
 
 #nteger variable
 age = 30
+height = 1.75
+is_active = true
+
+#constant
+PI = 3.14
 puts "Next year, age will be #{age}"
+puts "Height is #{height} m"
+puts "Is active? #{is_active}"
