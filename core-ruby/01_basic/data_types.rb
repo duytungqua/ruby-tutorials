e1fef15a84ecd22f1015ff89c9687c1aeefed84a
duytungqua <quangduy.hcmut@gmail.com>
2025-06-06 20:47:03 +0700

    feat: code data type

---
+++ core-ruby/01_basic/data_types.rb
@@ -0,0 +1,24 @@
+# String
+name = "Duy"
+address = "123 HCM"
+# Numbers
+age = 30
+height = 1.75
+
+# Boolean
+ is_adult = true
+# Array
+
+frruits = ["apple", "banana", "cherry"]
+
+# Hash
+
+person= {name: "Duy", age: 30, address: "123 HCM"}
+puts person[:name]
+
+# Range
+range = (1..10)
+puts range.to_a
+# Symbol
+
+# nil
