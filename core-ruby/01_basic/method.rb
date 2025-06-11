66a1e923f22165f44dee4c6c7fa7366f6d3f86fd
duytungqua <quangduy.hcmut@gmail.com>
2025-06-11 18:20:38 +0700

    tut: complete method

--- core-ruby/01_basic/method.rb
+++ core-ruby/01_basic/method.rb
@@ -4,3 +4,20 @@ def greet (name = "D")
 end
 greet("Duy")
 greet() # will use default value "D"
+
+# Method with parameters
+def add(a, b)
+  return a + b # you can use return to exit or implicitly return without return keyword
+end
+puts add(5, 10) # Output: 15
+
+#variable number of arguments
+def sum(*numbers)
+  total = 0
+  numbers.each do |num|
+    total += num
+  end
+
+  return total
+end
+puts sum(1, 2, 3, 4, 5) # Output: 15
