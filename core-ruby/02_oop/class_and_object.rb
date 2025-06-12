3b8f7a35ba39d3c3789c60569050996a14d2a052
duytungqua <quangduy.hcmut@gmail.com>
2025-06-12 16:26:33 +0700

    tut: update class instance and variable

--- core-ruby/02_oop/class_and_object.rb
+++ core-ruby/02_oop/class_and_object.rb
@@ -6,3 +6,19 @@ end
 
 dog = Dog.new
 dog.bark # Output: Woof!
+
+# Class with attributes and methods (@ prefix is instance variable)
+class Person 
+
+  def initial(name, age)
+    @name = name
+    @age = age
+  end
+  def greet
+    puts "Hello #{@name}, you are #{@age} years old."
+  end
+end
+
+p = Person.new
+p.initial("Duy", 30)
+p.greet # Output: Hello Duy, you are 30 years old.
