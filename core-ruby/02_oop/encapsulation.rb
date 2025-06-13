97cfecb8fc666e1c212f5ff571175004f9a93849
duytungqua <quangduy.hcmut@gmail.com>
2025-06-14 00:08:15 +0700

    tut: update encapsulation

--- core-ruby/02_oop/encapsulation.rb
+++ core-ruby/02_oop/encapsulation.rb
@@ -12,11 +12,22 @@ class Person
   #public method
   def display_info
     puts "Name: #{@name}, Age: #{@age}"
+    secret
   end
   #private method
   private
   def secret
     puts "This is a secret method."
   end
+  #protected method
+  protected
+  def protected_method
+    puts "This is a protected method."
+  end
 end
 
+p = Person.new("Duy", 30)
+p.name = "John" #   This will change the name to John
+p.greet # Output: Hello John, you are 30 years old.
+p.display_info # Output: Name: John, Age: 30
+p.protected_method
