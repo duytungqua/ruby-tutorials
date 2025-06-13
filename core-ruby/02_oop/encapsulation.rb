458ad1b56c3463ccb0a67cbf99ae8b47c5d4b952
duytungqua <quangduy.hcmut@gmail.com>
2025-06-14 00:21:18 +0700

    tut: move public method up protected

--- core-ruby/02_oop/encapsulation.rb
+++ core-ruby/02_oop/encapsulation.rb
@@ -14,6 +14,13 @@ class Person
     puts "Name: #{@name}, Age: #{@age}"
     secret
   end
+  def compare_names(other_person)
+    if self.protected_method == other_person.protected_method
+      puts "Both persons have the same name."
+    else
+      puts "The persons have different names."
+    end
+  end
   #private method
   private
   def secret
@@ -22,12 +29,15 @@ class Person
   #protected method
   protected
   def protected_method
-    puts "This is a protected method."
+    @name
   end
+
+  
 end
 
 p = Person.new("Duy", 30)
 p.name = "John" #   This will change the name to John
 p.greet # Output: Hello John, you are 30 years old.
 p.display_info # Output: Name: John, Age: 30
-p.protected_method
+p2 = Person.new("Jane", 25)
+p.compare_names(p2) # This will compare the names of p and p2 using the protected method
