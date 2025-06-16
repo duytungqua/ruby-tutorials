917f8aa95f6ff59f2333f4a9d91cea0e6cbe74f0
duytungqua <quangduy.hcmut@gmail.com>
2025-06-16 17:51:08 +0700

    tut: update modules and mixins

--- core-ruby/02_oop/modules_mixins.rb
+++ core-ruby/02_oop/modules_mixins.rb
@@ -7,7 +7,43 @@ end
 
 class Farewell
   include Greetings
+  extend Greetings
 end
 
-p = Greetings.new
+p = Farewell.new
 p.say_hello # Output: Hello from Greetings!
+Farewell.say_hello # Output: Hello from Greetings!
+# modules can be used to share methods across multiple classes 
+# include add module methods as instance methods
+# extend add module methods as class methods
+
+# Mixins with multiple class 
+module Walkable
+  def walk
+    puts "#{self.class} is walking."
+  end
+end
+
+class Dog
+  include Walkable
+  
+  def bark
+    puts "Woof!"
+  end
+end
+
+class Cat
+  include Walkable
+  
+  def meow
+    puts "Meow!"
+  end
+end
+
+dog = Dog.new
+dog.walk # Output: Dog is walking.
+dog.bark # Output: Woof!
+
+cat = Cat.new
+cat.walk # Output: Cat is walking.
+cat.meow # Output: Meow!
