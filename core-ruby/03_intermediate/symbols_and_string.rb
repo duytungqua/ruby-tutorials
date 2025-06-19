4cb573675938030a88e26398d8f7614dda103f8c
duytungqua <quangduy.hcmut@gmail.com>
2025-06-19 17:46:45 +0700

    tut: add new way to get hash value

--- core-ruby/03_intermediate/symbols_and_string.rb
+++ core-ruby/03_intermediate/symbols_and_string.rb
@@ -1,4 +1,11 @@
 # symbols are imutable strings symbols with prefix `:`
-person = :duy
-address = :123_hcm
-puts 
+
+puts "name".object_id
+
+puts :name.object_id
+
+hash = { name: "Alice", age: 30 }
+
+puts hash[:name]
+
+puts hash.fetch(:name, "Unknown") # Fetches value for key :name, returns "Unknown" if not found
