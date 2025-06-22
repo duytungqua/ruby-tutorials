d799aa4cdea626667b01e015d2652a11ea65d34f
duytungqua <quangduy.hcmut@gmail.com>
2025-06-23 06:58:12 +0700

    tut: custom class

--- core-ruby/03_intermediate/custom_class.rb
+++ core-ruby/03_intermediate/custom_class.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_acessor: :title, :author, :published_year
+  attr_accessor :title, :author, :published_year
   def initialize(title, author, published_year)
     @title = title
     @author = author
@@ -9,3 +9,12 @@ class Book
     puts "Title: #{@title}, Author: #{@author}, Published Year: #{@published_year}"
   end
 end
+
+Book1 = Book.new("The Great Gatsby", "F. Scott Fitzgerald", 1925)
+Book1.display_info # Output: Title: The Great Gatsby, Author: F. Scott Fitzgerald
+
+Book2 = Book.new("To Kill a Mockingbird", "Harper Lee", 1960)
+Book2.display_info # Output: Title: To Kill a Mockingbird, Author: Harper
+
+Book1.title = "The Great Gatsby (Updated)"
+Book1.display_info # Output: Title: The Great Gatsby (Updated), Author: F.
