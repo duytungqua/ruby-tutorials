d73449dd1f5fe8cad5cc017be3a4a1aa7d2d0236
duytungqua <quangduy.hcmut@gmail.com>
2025-06-25 16:55:01 +0700

    tut: complete module mixins

---
+++ core-ruby/03_intermediate/modules_and_mixins.rb
@@ -0,0 +1,47 @@
+# module mixins
+
+module Greetings
+  def say_hello
+    puts "Hello from Greetings!"
+  end
+end
+
+class Farewell
+  include Greetings
+  extend Greetings
+end
+
+p = Farewell.new
+p.say_hello # Output: Hello from Greetings!
+Farewell.say_hello # Output: Hello from Greetings!
+
+# Mixins with multiple classes
+module Walkable
+  def walk
+    puts "#{self.class} is walking."
+  end
+end
+
+class Dog
+  include Walkable
+  
+  def bark
+    puts "Woof!"
+  end
+end
+
+class Cat
+  include Walkable
+  
+  def meow
+    puts "Meow!"
+  end
+end
+
+dog = Dog.new
+dog.walk # Output: Dog is walking.
+dog.bark # Output: Woof!
+
+cat = Cat.new
+cat.walk # Output: Cat is walking.
+cat.meow # Output: Meow!
